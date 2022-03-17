a834026df121ade992d77090a065c56976b06be9
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-03-17 23:53:03 +0700

    kita ambil state yg telah berubah atau apapun yg diketik pada saat typing

--- src/components/BelajarBinding.js
+++ src/components/BelajarBinding.js
@@ -6,7 +6,7 @@ const BelajarBinding = () => {
   return (
     <div className="App">
       <header className="App-header"></header>
-
+      <h5>{text}</h5>
       <input type="text" className="text" placeholder="type here" onChange={(typing) => setText(typing.target.value)} />
     </div>
   );
