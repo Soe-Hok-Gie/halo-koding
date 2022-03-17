9d7da6089e84ed4383eca2846899c7236687021b
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-03-18 00:48:39 +0700

    penambahan komentar

--- src/components/BelajarBinding.js
+++ src/components/BelajarBinding.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 const BelajarBinding = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(""); //state untuk menampung data
 
   return (
     <div className="App">
       <header className="App-header"></header>
-      <h5>{text}</h5>
+      <h5>{text}</h5> {/*mengambil state yg telah diubah */}
       <input type="text" className="text" placeholder="type here" onChange={(typing) => setText(typing.target.value)} />
+      {/* Binding data */}
     </div>
   );
 };
