42f701906b1aa48ad9e020b6c46a8894a39e9926
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-03-18 00:01:22 +0700

    Copy Paste Script pada BelajarBinding sebelumnya

--- src/components/LogicBinding.js
+++ src/components/LogicBinding.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const LogicBinding = () => {
-  return <div>LogicBinding</div>;
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h5>{text}</h5>
+      <input type="text" className="text" placeholder="type here" onChange={(typing) => setText(typing.target.value)} />
+    </div>
+  );
 };
 
 export default LogicBinding;
