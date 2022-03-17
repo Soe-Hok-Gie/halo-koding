badd75fab985282c9171a8f842ba020f29315284
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-03-18 00:36:13 +0700

    membuat validasi utk menghilangkan gambar menggunakan logic ternary usState baru yg telah ditambahkan sebelumnya

--- src/components/LogicBinding.js
+++ src/components/LogicBinding.js
@@ -7,7 +7,7 @@ const LogicBinding = () => {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <img src="./assets/kupu-kupu.png" alt="" className="Web Developer" />
+      {showImage ? <img src="./assets/kupu-kupu.png" alt="" className="Web Developer" /> : ""}
       <h5>{text}</h5>
       <input type="text" className="text" placeholder="type here" onChange={(typing) => setText(typing.target.value)} />
     </div>
