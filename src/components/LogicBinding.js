76c36ffafc43c3bf8d201fe79fb2160bcbf1a332
Soe-Hok-Gie <wordcoffe0@gmail.com>
2022-03-18 00:46:24 +0700

    penambahan komentar

--- src/components/LogicBinding.js
+++ src/components/LogicBinding.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
 
 const LogicBinding = () => {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(""); //state pertama
 
-  const [showImage, setshowImage] = useState(false);
+  const [showImage, setshowImage] = useState(false); //state kedua
 
   useEffect(() => {
     text == "poem" ? setshowImage(true) : setshowImage(false);
-  }, [text]);
+  }, [text]); // logic memunculkan gambar jika dimasukkan kata-kata "poem"
 
   return (
     <div className="App">
       <header className="App-header"></header>
-      {showImage ? <img src="./assets/kupu-kupu.png" alt="" className="Web Developer" /> : ""}
+      {showImage ? <img src="./assets/kupu-kupu.png" alt="" className="Web Developer" /> : ""} {/*logic menghilangkan gambar */}
       <h5>{text}</h5>
       <input type="text" className="text" placeholder="type here" onChange={(typing) => setText(typing.target.value)} />
     </div>
